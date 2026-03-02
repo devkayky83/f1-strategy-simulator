d70e741abe163cdf5a3e4d898e910cf5ef28c014
Kayky Júnio <dev.kayky33@gmail.com>
2026-03-02 02:43:18 -0300

    feat: completing models archives and creating simulation engine

---
+++ backend/app/models/__init__.py
@@ -0,0 +1,5 @@
+from app.models.circuit import Circuit
+from app.models.tire_compound import TireCompound
+from app.models.strategy import Strategy
+from app.models.pit_stop import PitStop
+from app.models.simulation import Simulation
